b2b6eb9946815d9436510faa99312a7f77bee7e3
Leanne Clancy <s00170398@e1007-01.campus.itsligo.ie>
2017-03-02 11:45:36 +0000

    first commit

---
+++ TembooAccount.h
@@ -0,0 +1,5 @@
+#define TEMBOO_ACCOUNT "marloftitsligo"  // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "MyFirstApp"  // your Temboo app key name
+#define TEMBOO_APP_KEY  "TQm5JvRQNqoAp7k5XrmbgiOQjPo9bX1K"  // your Temboo app key
+
+
